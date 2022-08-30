54f7a3b4c6b0545797aa0475a9986770bf48d4e9
Ruhyat Ma'rullah <ruhyatmarullah@gmail.com>
2022-08-30 16:34:29 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
     <h1>HEllO WORLD</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius rem perferendis magnam. Sequi animi voluptates suscipit inventore tempore quidem ab sapiente, aperiam sit, nihil perferendis, exercitationem reprehenderit amet iste commodi.</p>
 </body>
 </html>
