92738447c2c0a84d471f22096f24eba4ee9928ba
Ruhyat Ma'rullah <ruhyatmarullah@gmail.com>
2022-08-30 16:39:25 +0700

    menambangkan sebuah list

--- index.php
+++ index.php
@@ -6,5 +6,8 @@
 <body>
     <h1>HEllO WORLD</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius rem perferendis magnam. Sequi animi voluptates suscipit inventore tempore quidem ab sapiente, aperiam sit, nihil perferendis, exercitationem reprehenderit amet iste commodi.</p>
+    <ul>
+        <li>satu</li>
+    </ul>
 </body>
 </html>
